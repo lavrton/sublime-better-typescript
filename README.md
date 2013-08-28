6f629b882a7817c282f1c8c122d9c1bf4b47611e
Лаврёнов Антон <lavrton@gmail.com>
2013-08-28 10:57:47 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,238 @@
+# Jump to Section
+
+* [Latest Change Log](#latest-changelog)
+* [FAQ](#faq)
+* [Installation](#installation)
+* [Updating](#updating)
+* [Commands/Shortcuts](#commandsshortcuts)
+* [Snippets](#snippets)
+* [Settings](#settings)
+
+# Overview
+
+**This package is for Sublime Text 3**.
+
+## Description
+
+This is fork of [Better CoffeeScript](https://github.com/aponxi/sublime-better-coffeescript) but adopted for work woth Typescript.
+
+# Installation
+
+## via Package Control
+
+> This is the recommended installation method.
+
+If you have Sublime Package Control, you know what to do. If not, well: it's a package manager for Sublime Text 3; it's awesome and you can [read about it here](https://sublime.wbond.net/). Installation guide can be [found here](https://sublime.wbond.net/installation).
+
+The simplest method of installation is through the Sublime Text console. The console is accessed via the ctrl+` shortcut or the View > Show Console menu. Once open, paste the appropriate Python code for your version of Sublime Text into the console.
+
+SUBLIME TEXT 3
+
+```
+import urllib.request,os; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); open(os.path.join(ipp, pf), 'wb').write(urllib.request.urlopen( 'http://sublime.wbond.net/' + pf.replace(' ','%20')).read())
+```
+
+This code creates the Installed Packages folder for you (if necessary), and then downloads the Package Control.sublime-package into it.
+
+After installing the package manager:
+
+* Open the Command Pallete (`ctrl+shift+P` or `cmd+shift+P`).
+* Type "Install Package" and hit return.
+* Type "Better TypeScript" and hit return.
+
+## via Source Control
+
+> If you plan to contribute, then you should install via this method. Otherwise it is recommended that you install the package via Package Control, see above.
+
+Sublime stores packages in the following locations:
+
+	Nix: ~/.config/sublime-text-3/packages
+	Mac: ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+	Win: %APPDATA%\Sublime Text 3\Packages
+
+### As a repository within the packages directory
+
+Open a Terminal/Console and run the following commands, replacing `PACKAGE_PATH` with the path corresponding to your OS above.
+
+	cd PACKAGE_PATH
+	git clone https://github.com/lavrton/sublime-better-typescript.git "Better TypeScript"
+
+
+#### A note on Package Control
+
+When Package Control tries to update your packages, if you have a repository in your packages directory then it will try to pull down and merge any changes. If you don't want this to happen and would rather handle everything yourself, then you can add the following to your settings (Preferences » Package Settings » Package Control » Settings - User):
+
+	"auto_upgrade_ignore": ["Better TypeScript"]
+
+# Updating
+
+If you are using Package Control, updating will be automatic and you don't have to worry about it.
+
+If using Source Control:
+
+	cd "PACKAGE_PATH/Better TypeScript"
+	git fetch origin
+	git merge origin/master
+
+# Commands/Shortcuts
+
+You can access the commands either using the command palette (`ctrl+shift+P` or `cmd+shift+P`) or via shortcuts.
+
+	alt+shift+s - Run a syntax check
+	alt+shift+c - Compile a file
+	alt+shift+d - Display compiled JavaScript
+	alt+shift+w - Toggle watch mode
+	alt+shift+p - Toggle output panel
+
+
+Context menu has `Compile Output` that compiles the current TypeScript and outputs the javascript code that is run, in a panel.
+
+**Note:** Some of the commands use the Status Bar for output, so you'll probably want to enable it (View » Show Status Bar).
+
+
+
+# Snippets
+
+- Use `TAB` to run a snippet after typing the trigger.
+- Use `TAB` and `shift+TAB` to cycle forward/backward through fields.
+- Use `ESC` to exit snippet mode.
+
+### Snippet Triggers
+
+Not snippets for now. Wait in new version.
+
+# Settings
+
+Go to `Preferences > Package Settings > Better TypeScript > Settings - User` to change settings.
+
+```Javascript
+{
+	/*
+		The directories you would like to include in $PATH environment variable.
+		Use this if your node installation is at a separate location and getting errors such as `cannot find node executable`
+
+		example:
+		"envPATH": "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+	*/
+	"envPATH": "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+	/*
+		The directory containing your typescript binary. Usually
+		/usr/local/bin.
+	*/
+	"binDir": "/usr/local/bin"
+	/*
+		Enable or disable refresh the compiled Output on Save.
+		Only available for watch mode.
+	*/
+,	"watchOnSave": true
+	/*
+		Enable refreshing compiled JS when CoffeeScript is modified.
+
+		Put false to disable
+		Put a number of seconds to delay the refresh
+	*/
+,	"watchOnModified": 0.5
+	/*
+		Enable Compiling on save. It will compile into the same folder.
+	*/
+,	"compileOnSave": true
+	/*
+		## Enable outputting the results of the compiled typescript in a panel
+	*/
+,	"showOutputOnSave": false
+	/*
+		## Enable compiling to a specific directory.
+		#### Description
+
+		if it is a string like 'some/directory' then `-o some/directory` will be added to `typescript` compiler.
+		if it is false or not string then it will compile your `script.ts` to the directory it is in.
+
+		#### Example:
+		Directory is relative to the file you are editing if specified such as
+			compileDir": "out"
+		Directory is absolute if specified such as
+			compileDir": "/home/logan/Desktop/out"
+
+	*/
+,	"compileDir": false
+	/*
+		## Enable compiling to a specific relative directories.
+
+		#### Example:
+		Set absolute path for compile dir:
+			"compileDir": "/home/user/projects/js"
+		And specified folders
+			"relativeDir": "/home/user/projects/ts"
+			"compilePaths":
+			{
+				"/home/user/projects/ts": "/home/user/projects/first/js",
+				"/home/user/projects/second/ts": "../js",
+			}
+
+		So
+			"/home/user/projects/ts/app.ts" will compile to "/home/user/projects/first/js/app.js"
+			"/home/user/projects/ts/models/prod.ts" will compile to "/home/user/projects/first/js/models/prod.js"
+			"/home/user/projects/ts/second/ts/app2.ts" will compile to "/home/user/projects/second/js/app2.js"
+			"/home/user/projects/main.ts" will compile to "/home/user/projects/js/main.js"
+
+	*/
+,	"compilePaths": false
+
+
+
+}
+
+
+```
+
+## Project settings
+
+Go to `Project > Edit Project` to change project settings.
+
+```Javascript
+{
+	"folders":
+	[
+		...
+	],
+	"settings":
+	{
+		"TypeScript":
+		{
+			"compileOnSave": true,
+			"compileDir": "out"
+		}
+	}
+}
+
+
+
+```
+
+# FAQ
+
+Most of the linux terminal commands written here can be run via [cygwin](http://cygwin.com/install.html) - aka Linux Terminal in Windows.
+
+- Most of the problems are related to configurations. Remember to configure `binDir` after you install!
+
+
+- Do I have `tsc` installed?
+
+Try finding tsc in your global npm list with `npm ls -g | grep tsc` which will output something like:
+
+```bash
+npm ls -g | grep tsc
+```
+
+
+- Where can I find out the path to tsc binary?
+
+In Linux `which` command will tell you where a command originates from. In terminal type:
+
+```bash
+which tsc
+# /usr/bin/tsc
+```
+
+This path will go into the `binDir` setting.
---
+++ TypeScript.py
@@ -0,0 +1,454 @@
+#aponxi
+import sys
+import os
+from os import path
+from subprocess import Popen, PIPE
+from sublime_plugin import TextCommand
+from sublime_plugin import WindowCommand
+import sublime_plugin
+import sublime
+import functools
+import locale
+import threading
+import tempfile
+from .sourcemap import load
+
+
+def settings_get(name, default=None):
+    # load up the plugin settings
+    plugin_settings = sublime.load_settings('TypeScript.sublime-settings')
+    # project plugin settings? sweet! no project plugin settings? ok, well promote plugin_settings up then
+    if sublime.active_window() and sublime.active_window().active_view():
+        project_settings = sublime.active_window().active_view().settings().get("TypeScript")
+    else:
+        project_settings = {}
+
+    # what if this isn't a project?
+    # the project_settings would return None (?)
+    if project_settings is None:
+        project_settings = {}
+
+    setting = project_settings.get(name, plugin_settings.get(name, default))
+    return setting
+
+
+def run(cmd, args=[], source="", cwd=None, env=None, callback=None):
+    """
+    Run command. "tsc", "cake", etc.
+    Will run on thread if callback function is passed.
+    """
+    if callback:
+        threading.Thread(target=lambda cb: cb(_run(cmd, args=args, source=source, cwd=cwd, env=env)), args=(callback,)).start()
+    else:
+        res = _run(cmd, args=args, source=source, cwd=cwd, env=env)
+        return res
+
+
+def _run(cmd, args=[], source="", cwd=None, env=None):
+    if not type(args) is list:
+        args = [args]
+    if sys.platform == "win32":
+        args = [cmd] + args
+        if sys.version_info[0] == 2:
+            for i in range(len(args)):
+                args[i] = args[i].encode(locale.getdefaultlocale()[1])
+        proc = Popen(args, env=env, cwd=cwd, stdout=PIPE, stdin=PIPE, stderr=PIPE, shell=True)
+        try:
+            stat = proc.communicate(input=source)
+        except:
+            stat = proc.communicate(input=source.encode("utf8"))
+        okay = proc.returncode == 0
+        return {"okay": okay, "out": stat[0].decode(locale.getdefaultlocale()[1]), "err": stat[1].decode(locale.getdefaultlocale()[1])}
+    else:
+        if env is None:
+            env = {"PATH": settings_get('binDir', '/usr/local/bin')}
+
+        # adding custom PATHs from settings
+        customEnv = settings_get('envPATH', "")
+        if customEnv:
+            env["PATH"] = env["PATH"]+":"+customEnv
+        if source == "":
+            command = [cmd] + args
+        else:
+            command = [cmd] + args + [source]
+        proc = Popen(command, env=env, cwd=cwd, stdout=PIPE, stderr=PIPE)
+        stat = proc.communicate()
+        okay = proc.returncode == 0
+        return {"okay": okay, "out": stat[0].decode('utf-8'), "err": stat[1].decode('utf-8')}
+
+
+def brew(args, source, cwd=None, callback=None):
+    """
+    Compile command
+    """
+    # if sys.platform == "win32":
+    #     args.append("-s")
+    # else:
+    #     args.append("-e")
+    tempTSFile = path.join(tempfile.gettempdir(), "temp.ts")
+    with open(tempTSFile, 'w') as f:
+        f.write(source)
+    res = run("tsc", [tempTSFile])
+    res["out"] = open(path.join(tempfile.gettempdir(), 'temp.js')).read()
+    return res
+
+
+def isTypescript(view=None):
+    if view is None:
+        view = sublime.active_window().active_view()
+    return 'source.ts' in view.scope_name(0)
+
+class Text():
+    @staticmethod
+    def all(view):
+        return view.substr(sublime.Region(0, view.size()))
+
+    @staticmethod
+    def sel(view):
+        text = []
+        for region in view.sel():
+            if region.empty():
+                continue
+            text.append(view.substr(region))
+        return "".join(text)
+
+    @staticmethod
+    def get(view):
+        text = Text.sel(view)
+        if len(text) > 0:
+            return text
+        return Text.all(view)
+
+
+class CompileCodeCommand(TextCommand):
+    def is_enabled(self):
+        return isTypescript(self.view)
+
+    def run(self, *args, **kwargs):
+        # no_wrapper = settings_get('noWrapper', True)
+        compile_dir = settings_get('compileDir')
+        source_file = self.view.file_name()
+        source_dir = os.path.normcase(os.path.dirname(source_file))
+        compile_paths = settings_get('compilePaths')
+        sourcemaps = settings_get('sourceMaps', True)
+
+        args = ['-c', source_file]
+        # if no_wrapper:
+        #     args = ['-b'] + args
+        if sourcemaps:
+            args = ['--sourcemap'] + args
+
+        # check instance of compile_paths
+        if isinstance(compile_paths, dict):
+            appendix_len = None
+            for key_path in compile_paths:
+                norm_path = os.path.normcase(key_path)
+                appendix = os.path.relpath(source_dir, norm_path)
+                if not appendix.startswith('..') and (appendix_len is None or len(appendix) < appendix_len):
+                    appendix_len = len(appendix)
+                    compile_dir = compile_paths[key_path]
+                    if not os.path.isabs(compile_dir):
+                        compile_dir = os.path.join(norm_path, compile_dir)
+                    compile_dir = os.path.join(compile_dir, appendix)
+
+        if compile_dir and (isinstance(compile_dir, str)):
+            # Check for absolute path or relative path for compile_dir
+            if not os.path.isabs(compile_dir):
+                compile_dir = os.path.join(source_dir, compile_dir)
+            print("Compile to:" + compile_dir)
+            # create folder if not exist
+            if not os.path.exists(compile_dir):
+                os.makedirs(compile_dir)
+                print("Compile dir did not exist, created folder: " + compile_dir)
+            folder, file_nm = os.path.split(source_file)
+            args = ['--outDir', compile_dir] + args
+        else:
+            compile_dir = source_dir
+            print("Compile to same directory")
+
+        if sourcemaps:
+            cwd = source_dir
+        else:
+            cwd = None
+        result = run("tsc", args=args, cwd=cwd)
+
+        if result['okay'] is True:
+            status = 'Compilation Succeeded'
+        else:
+            errorFirstLine = result['err'].splitlines()[0]
+            status = 'Compilation FAILED ' + errorFirstLine
+            sublime.error_message(errorFirstLine)
+
+        later = lambda: sublime.status_message(status)
+        sublime.set_timeout(later, 300)
+
+
+class CompileAndDisplayCodeCommand(TextCommand):
+    def is_enabled(self):
+        return isTypescript(self.view)
+
+    def run(self, edit, **kwargs):
+        no_wrapper = settings_get('noWrapper', True)
+        output = self.view.window().new_file()
+        output.set_scratch(True)
+        opt = kwargs["opt"]
+        if opt == '-p':
+            output.set_syntax_file('Packages/JavaScript/JavaScript.tmLanguage')
+        args = [opt]
+
+        if no_wrapper:
+            args = ['-b'] + args
+
+        res = brew(args, Text.get(self.view))
+        if res["okay"] is True:
+            output.insert(edit, 0, res["out"])
+        else:
+            output.insert(edit, 0, res["err"].split("\n")[0])
+
+
+class CheckCodeSyntaxCommand(TextCommand):
+    def is_enabled(self):
+        return isTypescript(self.view)
+
+    def run(self, edit):
+        # args = ['-b', '-p']
+        res = brew([], Text.get(self.view))
+        if res["okay"] is True:
+            status = 'Valid'
+        else:
+            status = res["err"].split("\n")[0]
+        sublime.message_dialog('Syntax %s' % status)
+
+
+class LintCodeCommand(TextCommand):
+
+    def is_enabled(self):
+        return isTypescript(self.view)
+
+    def run(self, edit):
+        filepath = self.view.file_name()
+        res = run("tsclint", args=[filepath, "--csv"])
+        error_list = []
+
+        for line in res["out"].split('\n'):
+            if not len(line.split(","))-1:
+                continue
+            lineNum = line.split(",")[1]
+            message = line.split(",")[-1]
+            try:
+                error_list.append({"message": message, "line": int(lineNum)-1})
+            except:
+                continue
+        if len(error_list):
+            self.popup_error_list(error_list)
+        else:
+            sublime.message_dialog("No lint errors.")
+
+    def popup_error_list(self, error_list):
+
+        panel_items = []
+
+        for error in error_list:
+            line_text = self.view.substr(self.view.full_line(self.view.text_point(error['line'], 0)))
+            item = [error['message'], '{0}: {1}'.format(error['line'] + 1, line_text.strip())]
+            panel_items.append(item)
+
+        def on_done(selected_item):
+            if selected_item == -1:
+                return
+
+            selected = self.view.sel()
+            selected.clear()
+
+            error = error_list[selected_item]
+            region_begin = self.view.text_point(error['line'], 0)
+
+            selected.add(sublime.Region(region_begin, region_begin))
+            # We have to force a move to update the cursor position
+            self.view.run_command('move', {'by': 'characters', 'forward': True})
+            self.view.run_command('move', {'by': 'characters', 'forward': False})
+            self.view.show_at_center(region_begin)
+
+        self.view.window().show_quick_panel(panel_items, on_done)
+
+watchers = {}
+
+
+def watched_filename(view):
+    if view.file_name() is not None:
+        filename = view.file_name().split('/')[-1]
+    else:
+        filename = "Unsaved File"
+    return filename
+
+
+class Tool():
+    @staticmethod
+    def get_file_name(file_path):
+        if file_path:
+            filename = os.path.split(file_path)[-1]
+        else:
+            filename = "Unsaved File"
+        return filename
+
+    @staticmethod
+    def get_js_file_name(tsc_file_name):
+        fileName, fileExtension = os.path.splitext(tsc_file_name)
+        output_filename = fileName + '.js'
+        return output_filename
+
+
+class Watcher():
+    def __init__(self, inputView):
+        self.inputView = inputView
+        print("Now watching " + watched_filename(inputView))
+        if self.inputView.window().num_groups() == 1:
+            # create new column
+            self.inputView.window().run_command('set_layout', {
+                "cols": [0.0, 0.5, 1.0],
+                "rows": [0.0, 1.0],
+                "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]
+            })
+        self.create_output()
+
+    def create_output(self):
+        self.sourceFilePath = self.inputView.file_name()
+        self.outputFileName = Tool.get_js_file_name(Tool.get_file_name(self.sourceFilePath))
+        self.outputTempDir = tempfile.gettempdir()
+        # print(self.outputTempDir)
+        self.outputFilePath = path.join(self.outputTempDir, self.outputFileName)
+
+        no_wrapper = settings_get('noWrapper', True)
+        args = []
+        if no_wrapper:
+            args = ['-b'] + args
+        args = args + ["--sourcemap", "--outDir", self.outputTempDir, self.sourceFilePath]
+
+        res = run("tsc", args)
+        if not res["okay"]:
+            sublime.message_dialog("Error. See console.")
+            print(res["err"])
+            return
+        # create new tab
+        self.outputView = self.inputView.window().open_file(self.outputFilePath)
+        # move it to second column
+        self.outputView.window().focus_group(1)
+        self.outputView.window().set_view_index(self.outputView, self.outputView.window().active_group(), 0)
+        # self.outputView.window().focus_group(0)
+        self.inputView.window().focus_view(self.inputView)
+
+    def refresh(self):
+        no_wrapper = settings_get('noWrapper', True)
+        args = ["--sourcemap", "--outDir", self.outputTempDir]
+        if no_wrapper:
+            args = ['-b'] + args
+        res = brew(args, source=Text.get(self.inputView))
+        with open(self.outputFilePath, 'w') as f:
+            f.write(res["out"])
+
+        mapFile = path.join(self.outputTempDir, self.outputFileName.split(".")[0]+'.js.map')
+        (inputRow, inputCol) = self.inputView.rowcol(self.inputView.sel()[0].begin())
+        index = load(open(mapFile)).getpos(line=inputRow, column=inputCol)
+        if not index:
+            return
+        (row, col) = index
+        row = int(row)
+
+        def goto():
+            selected = self.outputView.sel()
+            selected.clear()
+            region_begin = self.outputView.text_point(row, 0)
+            selected.add(sublime.Region(region_begin, region_begin))
+            self.outputView.run_command('move', {'by': 'characters', 'forward': True})
+            self.outputView.run_command('move', {'by': 'characters', 'forward': False})
+            self.outputView.show_at_center(region_begin)
+        sublime.set_timeout(goto, 10)
+
+    def stop(self):
+        if not self.inputView.id() in watchers:
+            return
+        print("Stop watching: " + self.inputView.file_name())
+        del watchers[self.inputView.id()]
+        window = self.outputView.window() or self.inputView.window()
+        if self.outputView.window():
+            window.focus_view(self.outputView)
+            window.run_command("close")
+
+        if len(watchers) == 0 and len(window.views_in_group(1)) == 0:
+            window.run_command('set_layout', {
+                "cols": [0.0, 1.0],
+                "rows": [0.0, 1.0],
+                "cells": [[0, 0, 1, 1]]
+            })
+
+
+class ToggleWatchMode(TextCommand):
+    views = {}
+    outputs = {}
+
+    def is_enabled(self):
+        return isTypescript(self.view)
+
+    def run(self, edit):
+        viewID = self.view.id()
+        if not viewID in watchers:
+            watchers[viewID] = Watcher(self.view)
+        else:
+            watchers[viewID].stop()
+
+
+class CaptureEditing(sublime_plugin.EventListener):
+
+    def is_enabled(self, view):
+        return isTypescript(view)
+
+    def handleTimeout(self, watcher):
+        if self._new_modify and not self._refreshed:
+            sublime.set_timeout(functools.partial(self.handleTimeout, watcher), 1000)
+            self._new_modify = False
+        else:
+            if self._refreshed:
+                return
+            self._refreshed = True
+            watcher.refresh()
+
+    def on_modified(self, view):
+        if not self.is_enabled(view):
+            return
+        viewID = view.id()
+        watch_modified = settings_get('watchOnModified')
+        self._new_modify = True
+        if watch_modified is not False and viewID in watchers:
+            self._refreshed = False
+            self.handleTimeout(watchers[viewID])
+
+    def on_post_save(self, view):
+        if not self.is_enabled(view):
+            return
+        compile_on_save = settings_get('compileOnSave', True)
+        if compile_on_save is True:
+            print("Compiling on save...")
+            view.run_command("compile")
+            show_compile_output_on_save = settings_get('showOutputOnSave', True)
+            if show_compile_output_on_save is True and isTypescript() is True and RunScriptCommand.PANEL_IS_OPEN is True:
+                print("Updating output panel...")
+                view.run_command("compile_output")
+
+        watch_save = settings_get('watchOnSave', True)
+        if watch_save:
+            viewID = view.id()
+            if viewID in watchers:
+                watchers[viewID].refresh()
+
+    def on_close(self, view):
+        viewID = view.id()
+        for k, watcher in watchers.items():
+            if watcher.outputView.id() == viewID:
+                watcher.stop()
+                break
+
+        if not self.is_enabled(view):
+            return
+
+        if viewID in watchers:
+            watchers[viewID].stop()
